2b7ac6bd23256b462b72c5dada50b10aa25a2b9e
Serangic <superpatrick.radulescu@gmail.com>
2026-04-10 20:09:48 +0200

    Lord praise the arrival of NeoVimm with theming

--- bash/.bashrc
+++ bash/.bashrc
@@ -6,5 +6,15 @@
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-PS1='[\u@\h \W]\$ '
+alias batinfo="~/bin/batinfo.sh"
+
+PS1=' [\[\e[1;34m\]\w\[\e[0m\]] $ '
+
 export TERMINAL=alacritty
+export PATH="$HOME/bin:$PATH"
+
+sysupdate() {
+	echo "Updating system and packages via Pacman..."
+	sudo pacman -Syu
+	echo -e "\e[1;32mUpdates have been completed.\e[0m"
+}
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,24 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.laststatus = 3
+
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#456585" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffffff", bold = true })
+
+vim.api.nvim_set_hl(0, "StatusLine", {
+  fg = "#d0d0d0",
+  bg = "#456585",
+})
+
+vim.api.nvim_set_hl(0, "StatusLineNC", {
+  fg = "#666666",
+  bg = "#1a1a1a",
+})
+
+vim.opt.statusline = "%f %= %l:%c %p%%"
