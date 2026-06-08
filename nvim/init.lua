b5c341479b728434edd0ca845919805d3c43a447
Serangic <superpatrick.radulescu@gmail.com>
2026-06-08 11:11:55 +0200

    Grayscalingv1

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,12 +28,12 @@ vim.opt.expandtab = true
 
 vim.opt.laststatus = 3
 
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#6085ab" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#a1c2e3", bold = true })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#4f4f4f" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ababab", bold = true })
 
 vim.api.nvim_set_hl(0, "StatusLine", {
-  fg = "#d0d0d0",
-  bg = "#456585",
+  fg = "#ffffff",
+  bg = "#3b3b3b",
 })
 
 vim.api.nvim_set_hl(0, "StatusLineNC", {
